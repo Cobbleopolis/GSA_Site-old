107c33c61809751142f7cda8c7dc75255cbee7e7
Cobbleopolis <agounimon@yahoo.com>
2014-12-11 17:01:14 -0500

    Broke Flags & Orentations

--- site/scripts/flag.js
+++ site/scripts/flag.js
@@ -2,30 +2,39 @@ var sexualityURL = 'https://spreadsheets.google.com/pub?key=12fWXwKd4Ta7scHhuU4o
 var genderURL = 'https://spreadsheets.google.com/pub?key=1M-3rloGazASnuSqzxwcxEcoTrAt6oucTOWP4uuSj-9U&hl=en&output=html';
 var colNum = 3;
 
-function getSexuality() {
+function fillSectionTitles () {
+    localStorage.clear();
     document.getElementById('sexualities').innerHTML += '<p class="flagSectionTitle">Sexualities</p>';
+    document.getElementById('genders').innerHTML += '<p class="flagSectionTitle">Genders</p>';
+}
+
+function getSexuality() {
+    //localStorage.clear();
     var sexualitySpreadsheet = new GoogleSpreadsheet();
     sexualitySpreadsheet.url(sexualityURL);
     sexualitySpreadsheet.load(function(result) {
-        for(var i = 0; i < result.data.length; i += 3){
+        document.getElementById("test").innerHTML = JSON.stringify(result);
+        for(var i = 0; i < result.data.length; i += colNum){
             document.getElementById('sexualities').innerHTML += '<p class="flagTitle">' + result.data[i] + ':</p><img class="flagImg" src = "' + result.data[i + 1] + '"><p>' + result.data[i + 2] + '</p>';
         }
     });
 }
 
 function getGender() {
-    document.getElementById('genders').innerHTML += '<p class="flagSectionTitle">Genders</p>';
-    var genderrSpreadsheet = new GoogleSpreadsheet();
-    genderrSpreadsheet.url(genderURL);
-    genderrSpreadsheet.load(function(result) {
-        for(var i = 0; i < result.data.length; i += 3){
+    //localStorage.clear();
+    var genderSpreadsheet = new GoogleSpreadsheet();
+    genderSpreadsheet.url(genderURL);
+    genderSpreadsheet.load(function(result) {
+        document.getElementById("test2").innerHTML = JSON.stringify(result);
+        for(var i = 0; i < result.data.length; i += colNum){
             document.getElementById('genders').innerHTML += '<p class="flagTitle">' + result.data[i] + ':</p><img class="flagImg" src = "' + result.data[i + 1] + '"><p>' + result.data[i + 2] + '</p>';
         }
     });
 }
 
 function showFlags() {
-    // localStorage.clear();
+    //localStorage.clear();
+    fillSectionTitles();
     getSexuality();
     getGender();
 }
