d0ada656ab22da709574290001f3b345703752ca
Cobbleopolis <agounimon@yahoo.com>
2014-12-10 21:39:17 -0500

    Working Navbar still needs to be refined

--- site/scripts/flag.js
+++ site/scripts/flag.js
@@ -1,5 +1,32 @@
 var flagURL = "https://spreadsheets.google.com/pub?key=12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s&hl=en&output=html";
 
+//{"url":"https://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic",
+//    "key":"12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s",
+//    "jsonUrl":"http://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic?alt=json-in-script",
+//    "googleUrl":{"sourceIdentifier":"https://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic",
+//    "url":"https://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic",
+//    "key":"12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s",
+//    "jsonCellsUrl":"http://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic?alt=json-in-script",
+//    "jsonListUrl":"http://spreadsheets.google.com/feeds/list/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic?alt=json-in-script",
+//    "jsonUrl":"http://spreadsheets.google.com/feeds/cells/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/basic?alt=json-in-script"},
+//    "data":["Gay (Homosexual-Male)",
+//    "Generally refers to a man who is attracted to men. Sometimes refers to all people who are attracted to people of the same sex; sometimes \"homosexual\" is used for this also,
+//    although this label is seen by many today as a medical term that should be retired from common use.",
+//    "images/flags/gay.png"]}
+
 function getFlag() {
-    
+    localStorage.clear();
+    var flagJSON;
+    var googleSpreadsheet = new GoogleSpreadsheet();
+    googleSpreadsheet.url(flagURL);
+    googleSpreadsheet.load(function(result) {
+        document.getElementById("test").innerHTML = JSON.parse(result).data[0];
+    });
+    //document.getElementById("test").innerHTML = flagJSON[0];
+}
+
+function showFlags() {
+    getFlag();
+    //alert("test");
+
 }
