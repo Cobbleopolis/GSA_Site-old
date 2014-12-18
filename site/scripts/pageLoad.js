5ccbfc49520cf5f5b2c37aaeece6117c668ef93d
Cobbleopolis <agounimon@yahoo.com>
2014-12-18 14:44:56 -0500

    F&O styling. Still need to add color

--- site/scripts/pageLoad.js
+++ site/scripts/pageLoad.js
@@ -5,4 +5,8 @@ function loadPageIndex(){
 function loadPageFlag(){
     navMake();
     showFlags();
+    $.getJSON("http://spreadsheets.google.com/feeds/list/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/values?alt=json", function(data) {
+        //first row "title" column
+        console.log(JSON.stringify(data));
+    });
 }
