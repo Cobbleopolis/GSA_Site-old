edec8d8daacf2c5a8aa3dda988a7062d409e6844
Cobbleopolis <agounimon@yahoo.com>
2014-12-24 00:55:11 -0500

    Flag load speed ++

--- site/scripts/pageLoad.js
+++ site/scripts/pageLoad.js
@@ -5,8 +5,4 @@ function loadPageIndex(){
 function loadPageFlag(){
     navMake();
     showFlags();
-    // $.getJSON("http://spreadsheets.google.com/feeds/list/12fWXwKd4Ta7scHhuU4o3RSUWNGZinF8pmOjIso3Ee8s/od6/public/values?alt=json", function(data) {
-    //     //first row "title" column
-    //     console.log(JSON.stringify(data));
-    // });
 }
