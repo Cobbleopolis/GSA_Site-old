b562d046addbce517aeb3e1216901bf7ddd2612b
Cobbleopolis <agounimon@yahoo.com>
2015-01-05 13:01:43 -0500

    Started work on the Events page.

--- site/scripts/pageLoad.js
+++ site/scripts/pageLoad.js
@@ -4,6 +4,8 @@ function loadPageIndex(){
 
 function loadPageEvents(){
     navMake();
+    document.getElementById("cal").height = window.innerHeight*0.95;
+    document.getElementById("cal").width = window.innerWidth*0.9;
 }
 
 function loadPageFlag(){
