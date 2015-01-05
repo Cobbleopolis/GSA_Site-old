9e6819017cadb9186053a3f7422810d53a77174f
Cobbleopolis <agounimon@yahoo.com>
2015-01-05 13:13:18 -0500

    Formatting on the Events page.

--- site/scripts/pageLoad.js
+++ site/scripts/pageLoad.js
@@ -4,8 +4,6 @@ function loadPageIndex(){
 
 function loadPageEvents(){
     navMake();
-    document.getElementById("cal").height = window.innerHeight*0.95;
-    document.getElementById("cal").width = window.innerWidth*0.9;
 }
 
 function loadPageFlag(){
