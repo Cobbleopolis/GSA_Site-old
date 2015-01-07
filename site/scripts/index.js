b8dbb8f02e2fa6416442f9042ff750d673c479a3
Cobbleopolis <cobbleopolis@gmail.com>
2015-01-07 13:26:35 -0500

    Major work on home page

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -1,18 +1,20 @@
 function makeSlideshow() {
-
-    fillSummery();
+    fillContent();
     fillSlideshow();
     activateSlideshow();
-
 }
 
-function fillSummery(){
+function fillContent() {
     $.getJSON("//spreadsheets.google.com/feeds/list/1ZSDcRkfQ8Mus6SIOp8qe0qsR3cTpGz6sJ7E4ZcZ0in0/od6/public/values?alt=json", function (data) {
-       for(var i = 0; i < data.feed.entry.length; i++){
-           if(data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'summery'){
-               document.getElementById('summery_content').innerHTML = data.feed.entry[i]['gsx$content']['$t'];
-           }
-       }
+        for (var i = 0; i < data.feed.entry.length; i++) {
+            if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'summery') {
+                document.getElementById('summery_content').innerHTML = '';
+                document.getElementById('summery_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
+            } else if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'about') {
+                document.getElementById('about_content').innerHTML = '';
+                document.getElementById('about_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
+            }
+        }
         $('#slideshow').height($('#summery_entry').outerHeight());
     });
 }
@@ -32,15 +34,16 @@ function activateSlideshow() {
 
 function fillSlideshow() {
     $.getJSON("//spreadsheets.google.com/feeds/list/1T7fxjhZnCayw4Go-pX4gAmuuTwtqvnf_glgcwahuHDE/od6/public/values?alt=json", function (data) {
-        var $slideshow = $('#slideshow')
+        var $slideshow = $('#slideshow');
+        var html = '';
         for (var i = 0; i < data.feed.entry.length; i++) {
-            var entry = '';
-            if (data.feed.entry[i]['gsx$type']['$t'].toLowerCase() === 'image') {
-                entry += '<div style="display: none;"><img src="' + data.feed.entry[i]['gsx$content']['$t'] + '" width = "' + $slideshow.width() + '" height = "' + $slideshow.height() + '"></div>';
-            } else if (data.feed.entry[i]['gsx$type']['$t'].toLowerCase() === 'text') {
-                entry += '<div style="display: none;">' + data.feed.entry[i]['gsx$content']['$t'] + '</div>';
+            if (i === 0) {
+                html += '<div style="display: block;">';
+            } else {
+                html += '<div style="display: none;">'
             }
-            $slideshow.append(entry);
+            html += '<img src="' + data.feed.entry[i]['gsx$content']['$t'] + '" width = "' + $slideshow.width() + '" height = "' + $slideshow.height() + '"></div>';
         }
+        document.getElementById('slideshow').innerHTML = html;
     });
 }
