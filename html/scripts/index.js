78f1d02fabb1adbcf663b38b6fc4103e568be41f
Cobbleopolis <cobbleopolis@gmail.com>
2015-01-13 16:38:49 -0500

    Got the image size fixed in the slideshow. Working on handling the Flags & Orientations page

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -1,29 +1,8 @@
 function makeHomePage() {
-    //fillContent();
-    //fillSlideshow();
-    activateSlideshow();
     $('#slideshow').height($('#summery_entry').outerHeight());
-}
-
-function fillContent() {
-    $.getJSON("//spreadsheets.google.com/feeds/list/1ZSDcRkfQ8Mus6SIOp8qe0qsR3cTpGz6sJ7E4ZcZ0in0/od6/public/values?alt=json", function (data) {
-        for (var i = 0; i < data.feed.entry.length; i++) {
-            if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'summery') {
-                //document.getElementById('summary_content').innerHTML = '';
-                //document.getElementById('summary_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
-            } else if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'about') {
-                //document.getElementById('about_content').innerHTML = '';
-                //document.getElementById('about_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
-            } else if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'meeting'){
-                //document.getElementById('meeting_content').innerHTML = '';
-                //document.getElementById('meeting_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
-            } else if (data.feed.entry[i]['gsx$section']['$t'].toLowerCase() === 'fishbowl') {
-                //document.getElementById('fishbowl_content').innerHTML = '';
-                //document.getElementById('fishbowl_content').innerHTML = '<p class="content_paragraph">' + data.feed.entry[i]['gsx$content']['$t'] + '</p>';
-            }
-        }
-        $('#slideshow').height($('#summery_entry').outerHeight());
-    });
+    $('.slideshow_img').attr( 'height', $('#slideshow').height());
+    $('.slideshow_img').attr( 'width', $('#slideshow').width());
+    activateSlideshow();
 }
 
 function activateSlideshow() {
@@ -37,20 +16,4 @@ function activateSlideshow() {
             .appendTo('#slideshow');
     }, 3000);
 
-}
-
-function fillSlideshow() {
-    $.getJSON("//spreadsheets.google.com/feeds/list/1T7fxjhZnCayw4Go-pX4gAmuuTwtqvnf_glgcwahuHDE/od6/public/values?alt=json", function (data) {
-        var $slideshow = $('#slideshow');
-        var html = '';
-        for (var i = 0; i < data.feed.entry.length; i++) {
-            if (i === 0) {
-                html += '<div style="display: block">';
-            } else {
-                html += '<div style="display: none">';
-            }
-            html += '<img src="' + data.feed.entry[i]['gsx$content']['$t'] + '" width = "' + $slideshow.width() + '" height = "' + $slideshow.height() + '"></div>';
-        }
-        //document.getElementById('slideshow').innerHTML = html;
-    });
 }
--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@
     var fs = require('fs');
     var cheerio = require('cheerio');
     var databaseUrl = 'gsa-site:gayisok1@99.62.101.62:25566/gsa-site'; // 'username:password@example.com/mydb'
-    var collections = ['names', 'homePage'];
+    var collections = ['homePage', 'sexualities', 'romantic', 'genders', 'other_terms'];
     var db = require('mongojs').connect(databaseUrl, collections);
 
     var navBar = fs.readFileSync(__dirname + '/html/resorcePages/navBar.html');
@@ -10,25 +10,12 @@
 
 
     module.exports.indexHandle = function (url, res) {
-        //console.log(__dirname);
         fs.readFile(url, function (err, file) {
             var $ = cheerio.load(file);
             $('#navBar').html(navBar);
             $('#navButton').html(navButton);
 
             var homePage = db.collection('homePage');
-            //var slideshowHTML = '';
-            //var files = fs.readdirSync('/html/images/slideshow');
-            //for (var i = 0; i < files.length; i++) {
-            //    console.log("File:" + files[i]);
-            //    if (i === 0) {
-            //        slideshowHTML += '<div style="display: block">';
-            //    } else {
-            //        slideshowHTML += '<div style="display: none">';
-            //    }
-            //    slideshowHTML += '<img src="' + files[i] + '" width = "' + $('#slideshow').width() + '" height = "' + $('#slideshow').height() + '"></div>';
-            //}
-            //$('#slideshow').html(slideshowHTML);
             fs.readdir('html/images/slideshow', function (err, files) {
                 var html = '';
                 if (!err)
@@ -39,7 +26,7 @@
                         } else {
                             html += '<div style="display: none">';
                         }
-                        html += '<img src="images/flags/' + files[i] + '"></div>';
+                        html += '<img class="slideshow_img" src="images/flags/' + files[i] + '"></div>';
                     }
                 $('#slideshow').html(html);
             });
@@ -66,6 +53,25 @@
         });
     };
 
+    module.exports.flagHandle = function (url, res) {
+        fs.readFile(url, function (err, file) {
+            var $ = cheerio.load(file);
+            $('#navBar').html(navBar);
+            $('#navButton').html(navButton);
+
+            var sexualities = db.collection('sexualities');
+            var romantic = db.collection('romantic');
+            var genders = db.collection('genders');
+            var other_terms = db.collection('other_terms');
+            sexualities.find(function (err, flag) {
+                if (!err)
+                    for (var i = 0; i < flag.length; i++) {
+                        //TODO Insert the flags
+                    }
+                res.send($.html());
+            });
+        });
+    };
 
 }());
 
