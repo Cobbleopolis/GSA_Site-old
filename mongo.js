f5fec34ee657f482e1e27abcf13e32ba5dcab1eb
Cobbleopolis <cobbleopolis@gmail.com>
2015-01-13 18:33:17 -0500

    Got the image size fixed in the slideshow. Working on handling the Flags & Orientations page

--- mongo.js
+++ mongo.js
@@ -11,6 +11,8 @@
 
     module.exports.indexHandle = function (url, res) {
         fs.readFile(url, function (err, file) {
+            if (err)
+                throw err;
             var $ = cheerio.load(file);
             $('#navBar').html(navBar);
             $('#navButton').html(navButton);
@@ -18,36 +20,38 @@
             var homePage = db.collection('homePage');
             fs.readdir('html/images/slideshow', function (err, files) {
                 var html = '';
-                if (!err)
-                    for (var i = 0; i < files.length; i++) {
-                        //console.log("File:" + files[i]);
-                        if (i === 0) {
-                            html += '<div style="display: block">';
-                        } else {
-                            html += '<div style="display: none">';
-                        }
-                        html += '<img class="slideshow_img" src="images/slideshow/' + files[i] + '"></div>';
+                if (err)
+                    throw err;
+                for (var i = 0; i < files.length; i++) {
+                    //console.log("File:" + files[i]);
+                    if (i === 0) {
+                        html += '<div style="display: block">';
+                    } else {
+                        html += '<div style="display: none">';
                     }
+                    html += '<img class="slideshow_img" src="images/slideshow/' + files[i] + '"></div>';
+                }
                 $('#slideshow').html(html);
             });
             homePage.find(function (err, docs) {
-                if (!err)
-                    for (var i = 0; i < docs.length; i++) {
-                        switch (docs[i].section) {
-                            case 'summary':
-                                $('#summary_content').html(docs[i].content);
-                                //$('#slideshow').height($('#summery_entry').outerHeight());
-                                break;
-                            case 'about':
-                                $('#about_content').html(docs[i].content);
-                                break;
-                            case 'meetings':
-                                $('#meeting_content').html(docs[i].content);
-                                break;
-                            case 'fishbowl':
-                                $('#fishbowl_content').html(docs[i].content);
-                        }
+                if (err)
+                    throw err;
+                for (var i = 0; i < docs.length; i++) {
+                    switch (docs[i].section) {
+                        case 'summary':
+                            $('#summary_content').html(docs[i].content);
+                            //$('#slideshow').height($('#summery_entry').outerHeight());
+                            break;
+                        case 'about':
+                            $('#about_content').html(docs[i].content);
+                            break;
+                        case 'meetings':
+                            $('#meeting_content').html(docs[i].content);
+                            break;
+                        case 'fishbowl':
+                            $('#fishbowl_content').html(docs[i].content);
                     }
+                }
                 res.send($.html());
             });
         });
@@ -55,6 +59,8 @@
 
     module.exports.flagHandle = function (url, res) {
         fs.readFile(url, function (err, file) {
+            if (err)
+                throw err;
             var $ = cheerio.load(file);
             $('#navBar').html(navBar);
             $('#navButton').html(navButton);
@@ -64,20 +70,24 @@
             var genders = db.collection('genders');
             var other_terms = db.collection('other_terms');
             sexualities.find(function (err, flags) {
-                if (!err)
-                    $("#sexualitiesSection").html(flagToHTML(flags));
+                if (err)
+                    throw err;
+                $("#sexualitiesSection").html(flagToHTML(flags));
             });
             romantic.find(function (err, flags) {
-                if (!err)
-                    $("#romanticSection").html(flagToHTML(flags));
+                if (err)
+                    throw err;
+                $("#romanticSection").html(flagToHTML(flags));
             });
             genders.find(function (err, flags) {
-                if (!err)
-                    $("#gendersSection").html(flagToHTML(flags));
+                if (err)
+                    throw err;
+                $("#gendersSection").html(flagToHTML(flags));
             });
             other_terms.find(function (err, flags) {
-                if (!err)
-                    $("#otherTermsSection").html(flagToHTML(flags));
+                if (err)
+                    throw err;
+                $("#otherTermsSection").html(flagToHTML(flags));
                 res.send($.html());
             });
 
@@ -86,6 +96,8 @@
 
     module.exports.eventsHandle = function (url, res) {
         fs.readFile(url, function (err, file) {
+            if (err)
+                throw err;
             var $ = cheerio.load(file);
             $('#navBar').html(navBar);
             $('#navButton').html(navButton);
@@ -95,6 +107,8 @@
 
     module.exports.chatHandle = function (url, res) {
         fs.readFile(url, function (err, file) {
+            if (err)
+                throw err;
             var $ = cheerio.load(file);
             $('#navBar').html(navBar);
             $('#navButton').html(navButton);
@@ -102,14 +116,14 @@
         });
     };
 
-    function flagToHTML(array){
+    function flagToHTML(array) {
         var html = '';
         for (var i = 0; i < array.length; i++) {
             html += '<div class="flagEntry">' +
             '<div class="ui top attached header">' +
             '<p class="flagTitle">' + array[i].identification + '</p>' +
             '</div>';
-            if(array[i].image_link){
+            if (array[i].image_link) {
                 html += '<div class="ui attached segment">' +
                 '<img class="flagImg" src = "' + array[i].image_link + '">' +
                 '</div>';
@@ -117,7 +131,7 @@
             html += '<div class="ui attached segment">' +
             '<p class="flagDesc">' + array[i].description + '</p>' +
             '</div>';
-            if(array[i].warning){
+            if (array[i].warning) {
                 html += '<div class="ui bottom attached warning message">' +
                 '<i class="warning sign icon"></i>' + array[i].warning +
                 '</div>';
