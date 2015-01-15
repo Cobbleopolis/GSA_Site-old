8cca2988c557aac2660c04ae51c622a526dcde30
Cobbleopolis <agounimon@yahoo.com>
2015-01-14 19:53:14 -0500

    Reworked the slideshow on the home page.

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -1,7 +1,6 @@
 function makeHomePage() {
     $('#slideshow_entry').outerHeight($('#summery_entry').outerHeight());
     $('.slideshow_img').attr( 'height', $('#slideshow_entry').height());
-    //$('.slideshow_img').attr( 'width', $('#slideshow').width());
     activateSlideshow();
 }
 
