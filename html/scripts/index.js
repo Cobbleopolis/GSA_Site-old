ac5323fa83eea197f249e5afd2bb3d58505a4eb3
Cobbleopolis <agounimon@yahoo.com>
2015-03-08 01:50:12 -0500

    changed index.js

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -4,4 +4,16 @@ function makeHomePage() {
     activateSlideshow();
 }
 
+var animTime = 500;
 
+function activateSlideshow() {
+    $(function() {
+        $("#slideshow_entry > div:gt(0)").hide();
+        setInterval(function() {
+            $('#slideshow_entry > div:first').fadeOut(animTime);
+            $('#slideshow_entry > div:first').next().delay(animTime).fadeIn(animTime);
+            $('#slideshow_entry > div:first').appendTo("#slideshow_entry");
+        }, 4000);
+    });
+
+}
