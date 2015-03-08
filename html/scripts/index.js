b2b8e7e462fa0957098bb1b37ce45973133dc462
Cobbleopolis <agounimon@yahoo.com>
2015-03-08 01:33:26 -0500

    fixed slideshow, formatting and cleaned up code.

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -5,12 +5,13 @@ function makeHomePage() {
 }
 
 function activateSlideshow() {
-    $(function(){
-        $('#slideshow_entry div:gt(0)').hide();
-        setInterval(function(){
-                $('#slideshow_entry div:first-child').fadeOut(1000)
-                    .next().fadeIn(1000)
-                    .end().appendTo('#slideshow_entry');},
-            4000);
+    $(function() {
+        $("#slideshow_entry > div:gt(0)").hide();
+        setInterval(function() {
+            $('#slideshow_entry > div:first').fadeOut(1000);
+            $('#slideshow_entry > div:first').next().delay(1000).fadeIn(1000);
+            $('#slideshow_entry > div:first').appendTo("#slideshow_entry");
+        }, 3000);
     });
+
 }
