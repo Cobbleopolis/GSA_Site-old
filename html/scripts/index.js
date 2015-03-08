9e6ae81e3313e18c057b7df6481e5626e37fda83
U-TARDISBLUE\Logan <Logan@TardisBlue.(none)>
2015-03-08 01:38:36 -0500

    changed time in animation

--- html/scripts/index.js
+++ html/scripts/index.js
@@ -4,14 +4,16 @@ function makeHomePage() {
     activateSlideshow();
 }
 
+var animTime = 500;
+
 function activateSlideshow() {
     $(function() {
         $("#slideshow_entry > div:gt(0)").hide();
         setInterval(function() {
-            $('#slideshow_entry > div:first').fadeOut(1000);
-            $('#slideshow_entry > div:first').next().delay(1000).fadeIn(1000);
+            $('#slideshow_entry > div:first').fadeOut(animTime);
+            $('#slideshow_entry > div:first').next().delay(animTime).fadeIn(animTime);
             $('#slideshow_entry > div:first').appendTo("#slideshow_entry");
-        }, 3000);
+        }, 4000);
     });
 
 }
